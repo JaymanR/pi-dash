15db81892fcad61aa58473ccd1e63ba2e2136bb1
Jayman Rana <111602593+JaymanR@users.noreply.github.com>
2024-08-10 01:45:40 -0400

    Delete build directory

--- desktop-client/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/CMakeFiles/desktop-client.dir/desktop-client_autogen/EWIEGA46WW/qrc_resources.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/desktop-client.dir/desktop-client_autogen/EWIEGA46WW/qrc_resources.cpp.obj: \
- E:\Projects\QT\pi-dash\desktop-client\build\Desktop_Qt_6_7_2_MinGW_64_bit-Debug\desktop-client_autogen\EWIEGA46WW\qrc_resources.cpp
